586370ce41f63b584e3eba66e0ac2214ec837197
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-12-26 19:21:26 +0300

    Settings buttons added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,65 @@ public class UIManager : MonoBehaviour
 {
     public Image whiteeffectimage;
     private int effectcontrol = 0;
+
+
+
+    public Animator LayoutAnimator;
+
+
+    public GameObject settings_open;
+    public GameObject settings_close;
+    public GameObject sound_On;
+    public GameObject sound_Off;
+    public GameObject vibration_On;
+    public GameObject vibration_Off;
+    public GameObject iap;
+    public GameObject information;
+
+    
+    public void Settings_open()
+    {
+        settings_open.SetActive(false);
+        settings_close.SetActive(true);
+        LayoutAnimator.SetTrigger("Slide_in");
+    }
+    public void Settings_close()
+    {
+        settings_open.SetActive(true);
+        settings_close.SetActive(false);
+        LayoutAnimator.SetTrigger("Slide_out");
+    }
+
+    public void Sound_On()
+    {
+        sound_On.SetActive(false);
+        sound_Off.SetActive(true);
+        
+    }
+    public void Sound_Off()
+    {
+        sound_On.SetActive(true);
+        sound_Off.SetActive(false);
+        
+    }
+    public void Vibration_On()
+    {
+        vibration_On.SetActive(false);
+        vibration_Off.SetActive(true);
+        
+    }
+    public void Vibration_Off()
+    {
+        vibration_On.SetActive(true);
+        vibration_Off.SetActive(false);
+
+    }
+    
+
+
+
+
+
     public IEnumerator WhiteEffect()
     {
         whiteeffectimage.gameObject.SetActive(true);
